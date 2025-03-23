548022d18458315ea2d8dadfb6e09ae67d4351fb
unknown <golovinov817@gmail.com>
2025-03-23 19:23:59 +0100

    lesson one

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,51 +1,15 @@
-import ContactList from '../ContactList/ContactList';
-import ContactForm from '../ContactForm/ContactForm';
-import SearchBox from '../SearchBox/SearchBox';
+import { useEffect } from "react";
+import axios from "axios";
 
-import {useEffect, useState } from 'react';
-import  useContacts  from '../../useContacts.json';
-import css from './App.module.css';
+export default function App ()  {
 
-const LS_CONTACTS_KEY = 'initial-contacts';
+  useEffect(() => {
+    // Тут будемо виконувати HTTP-запит
+  }, []);
 
-
-const initialContacts = () => {
-	const localStorageContacts = localStorage.getItem(LS_CONTACTS_KEY);
-	return localStorageContacts ? JSON.parse(localStorageContacts) : useContacts;
-};
-
-
- export default function App () {
-  const [contacts, setContacts] = useState(initialContacts);
-  const [filter, setFilter] = useState('');
-
-  
-	useEffect(() => {
-		localStorage.setItem(LS_CONTACTS_KEY, JSON.stringify(contacts));
-	}, [contacts]);
-
-  const handleAddContact = (newContact) => {
-    setContacts((prevContact) => {
-      return [...prevContact, newContact];
-    });
-  };
-
-  const handleDeleteContacts = (Id) => {
-    setContacts((prevContact) => {
-      return prevContact.filter((contact) => contact.id !== Id);
-    });
-  };
-
-  const visibleContact = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-  
   return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm onAdd={handleAddContact} />
-      <SearchBox value={filter} onFilter={setFilter}/>
-      <ContactList contacts={visibleContact} onDelete={handleDeleteContacts} />
+    <div>
+      <h1>Latest articles</h1>
     </div>
   );
- };
+};
