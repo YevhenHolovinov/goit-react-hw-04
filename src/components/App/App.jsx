e3b814edbae1d87bfb6a8d6d9577696b53a91fda
unknown <golovinov817@gmail.com>
2025-03-26 21:19:47 +0100

    lesson home work 4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,43 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import  ArticleList from '../ArticleList/ArticleList';
+import { fetchArticlesWithTopic } from "../../components/Articles-api/Articles-api";
 
-export default function App ()  {
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    // Тут будемо виконувати HTTP-запит
+    async function fetchArticles() {
+      try{
+        setLoading(true);
+        const data = await fetchArticlesWithTopic("react");
+        setArticles(data);
+      } catch (error) {
+        setError(true);
+
+      }finally {
+        setLoading(false);
+      }
+     
+    }
+
+    fetchArticles();
   }, []);
 
   return (
     <div>
       <h1>Latest articles</h1>
+      {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
 };
+
+
+
+
+export default App;
