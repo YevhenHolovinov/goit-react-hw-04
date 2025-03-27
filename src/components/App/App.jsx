45efe517a3dd51d12d73251f3b830cd6fc515e5d
unknown <golovinov817@gmail.com>
2025-03-27 21:21:28 +0100

    lesson home work useRef

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,43 +1,154 @@
-import { useEffect, useState } from "react";
-import  ArticleList from '../ArticleList/ArticleList';
-import { fetchArticlesWithTopic } from "../../components/Articles-api/Articles-api";
+// import { useEffect, useState } from "react";
+// import  ArticleList from '../ArticleList/ArticleList';
+// import { fetchArticlesWithTopic } from "../../components/Articles-api/Articles-api";
+// import SearchForm from '../SearchForm/SearchForm';
 
-const App = () => {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+//   const handleSearch = async (topic) => {
+//     try {
+//       setArticles([]);
+//       setError(false);
+//       setLoading(true);
+//       const data = await fetchArticlesWithTopic(topic);
+//       setArticles(data);
+//     } catch (error) {
+//       setError(true);
+//     } finally {
+//       setLoading(false);
+//     }
 
-  useEffect(() => {
-    async function fetchArticles() {
-      try{
-        setLoading(true);
-        const data = await fetchArticlesWithTopic("react");
-        setArticles(data);
-      } catch (error) {
-        setError(true);
 
-      }finally {
-        setLoading(false);
-      }
-     
-    }
+//   };
 
-    fetchArticles();
-  }, []);
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+//       <SearchForm onSearch={handleSearch} />
+//       {loading && <p>Loading data, please wait...</p>}
+//       {error && (
+//         <p>Whoops, something went wrong! Please try reloading this page!</p>
+//       )}
+
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
+///////////////////////////////////////
+
+// import { useEffect, useState, useRef } from "react";
+
+// const App = () => {
+//   const [value, setValue] = useState(0);
+//   const btnRef = useRef();
+
+//   // Буде undefined на першому рендері
+//   // і посиланням на DOM-елемент всі наступні
+//   console.log("App: ", btnRef.current);
+
+//   useEffect(() => {
+//     // Ефект виконується після монтування,
+//     // тому завжди буде посиланням на DOM-елемент
+//     console.log("useEffect: ", btnRef.current);
+//   });
+
+//   const handleClick = () => {
+//     // Кліки будуть після монтування,
+//     // тому завжди буде посиланням на DOM-елемент
+//     console.log("handleClick: ", btnRef.current);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={() => setValue(value + 1)}>
+//         Update value to trigger re-render
+//       </button>
+//       <button ref={btnRef} onClick={handleClick}>
+//         Button with ref
+//       </button>
+//     </>
+//   );
+// };
+
+////////////////////////////////////////////////////
+
+// import { useEffect, useRef } from "react";
+
+// const App = () => {
+//   const valueRef = useRef(0);
+
+//   useEffect(() => {
+//     // Виконається лише один раз під час монтування.
+//     // Наступні оновлення значення рефа не
+//     // викличуть оновлення компонента
+//     console.log(valueRef.current);
+//   });
+
+//   const handleClick = () => {
+//     valueRef.current += 1;
+//   };
+
+//   return <button onClick={handleClick}>Click to update ref value</button>;
+// };
+
+////////////////////////////////////////////
+
+import { useRef } from "react";
+
+const Player = ({ source }) => {
+  const playerRef = useRef();
+
+  const play = () => playerRef.current.play();
+
+  const pause = () => playerRef.current.pause();
 
   return (
     <div>
-      <h1>Latest articles</h1>
-      {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
-      {error && (
-        <p>Whoops, something went wrong! Please try reloading this page!</p>
-      )}
-      {articles.length > 0 && <ArticleList items={articles} />}
+      <video ref={playerRef} src={source}>
+        Sorry, your browser does not support embedded videos.
+      </video>
+      <div>
+        <button onClick={play}>Play</button>
+        <button onClick={pause}>Pause</button>
+      </div>
     </div>
   );
 };
 
+const App = () => {
+  return <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />;
+};
 
+///////////////////////////
 
+// ComponentA.jsx
+// import { useToggle } from "../hooks/useToggle.js";
+
+// const ComponentA = () => {
+//   const { isOpen, open, close } = useToggle();
+
+//   return (
+//     <>
+//       <button onClick={open}>Open modal</button>
+//       <Modal isOpen={isOpen} onClose={close} />
+//     </>
+//   );
+// };
+
+// ComponentB.jsx
+// import { useToggle } from "../hooks/useToggle.js";
+
+// const ComponentB = () => {
+//   const { isOpen, open, close } = useToggle();
+
+//   return (
+//     <>
+// 	  <button onClick={open}>Open sidebar</button>
+//       <Sidebar isOpen={isOpen} onClose={close} />
+//     </>
+//   );
+// };
 
 export default App;
